7465b47a0d8857116d0cc29f50920990244c1414
Ennio-M <marrese.ennio@gmail.com>
2022-05-06 17:57:09 +0200

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img src="../assets/imgs/logo.png" alt="Logo Spotify" class="my-2 mx-4">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/variables.scss';
+    header{
+        background-color: $card-bg;
+        img{
+            width: 50px;
+        }
+    }
+</style>
