8bcba0c72e01c2038ab68e841ee366aba3582cf0
Ennio-M <marrese.ennio@gmail.com>
2022-05-11 08:29:50 +0200

    aggiunta select generi dischi

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,8 +2,9 @@
     <main>
         <div class="container">
             <app-loader v-if="loading"/>
+            <app-search @mySearch="setSearchText" :genresList="allGenres"/>
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 gy-3">
-                <div v-for="(item, index) in links" :key="index" class="col">
+                <div v-for="(item, index) in filteredList" :key="index" class="col">
                     <app-card :album="item"/>
                 </div>
             </div>
@@ -14,24 +15,41 @@
 <script>
 import AppCard from './AppCard.vue';
 import AppLoader from './AppLoader.vue';
+import AppSearch from './AppSearch.vue'
 import axios from 'axios';
 
 export default {
     name: 'AppMain',
     components: {
         AppCard,
-        AppLoader
+        AppLoader,
+        AppSearch
     },
     data() {
         return {
             links: [],
-            loading: false
+            loading: false,
+            searchText: '',
+            allGenres: []
+        }
+    },
+    methods: {
+        setSearchText(txt) {
+            this.searchText = txt;
+        }
+    },
+    computed: {
+        filteredList() {
+            return this.links.filter((el) => el.genre.includes(this.searchText));
         }
     },
     mounted() {
         this.loading = true
         axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((res) => {
             this.links = res.data.response;
+            this.links.forEach((el) => {
+                !this.allGenres.includes(el.genre) ? this.allGenres.push(el.genre) : '';
+            })
         }).catch((error) => {
             console.log(error);
         }).finally(() => {
@@ -46,7 +64,7 @@ export default {
 
     main{
         background-color: $bg-dark;
-        padding: 50px 0;
+        padding-bottom: 50px;
         min-height: calc(100% - 82px);
     }
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="input-group py-3">
+            <p class="text-white m-0">Seleziona un genere</p>
+            <select name="genres" id="genres" v-model="inputText" @change="search" class="mx-3 p-2 rounded-3">
+                <option value="">Tutti</option>
+                <option :value="genre" v-for="(genre, index) in genresList" :key="index">{{genre}}</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppSearch',
+    data() {
+        return {
+            inputText: ''
+        }
+    },
+    props: {
+        genresList: Array
+    },
+    methods: {
+        search() {
+            this.$emit('mySearch', this.inputText);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    p{
+        line-height: 37px;
+        font-size: 1.2em;
+    }
+    select{
+        font-size: 1.2em;
+    }
+</style>
